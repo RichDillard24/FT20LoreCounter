08339148f8da0173fbab4f01f1c3434661b447d4
RichDillard24 <richiegd@hotmail.com>
2026-03-20 10:00:19 -0700

    Initial Commit

---
+++ FT20LoreCounter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FT20LoreCounter
+//
+//  Created by Richard Dillard on 3/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FT20LoreCounter/FT20LoreCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FT20LoreCounterApp.swift
+//  FT20LoreCounter
+//
+//  Created by Richard Dillard on 3/20/26.
+//
+
+import SwiftUI
+
+@main
+struct FT20LoreCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
